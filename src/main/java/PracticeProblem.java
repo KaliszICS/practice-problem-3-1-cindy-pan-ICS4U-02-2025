4d68020416e6ed9df81685cd0b3634a9afd0562b
cindy pan <cindy.pan@student.tdsb.on.ca>
2025-04-01 14:16:12 +0000

    Update

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,9 +1,36 @@
 public class PracticeProblem {
 
-	public static void main(String args[]) {
-
+	public static int sumOfDigits(int x){
+	String temp = Integer.toString(x);
+	int index =0;
+	if (x<0){
+		x = x*(-1);
 	}
 
+
+	if (index>= temp.length()){
+		return 0;
+	} 
+	else if (temp.length() ==1){
+		return x;
+	}
+	else {
+		return (x%10) + sumOfDigits(x/10);
+	}
+	}
+
+
+	public static int factorial(int n){
+		if (n<0){
+			return 0;
+		}
+		else if (n>0){
+			return n * factorial(n-1);
+		}
+		else {
+			return 1;
+		}
+	}
 	
 
 }
